211a6213af7c467fe2a9cb69467a123be6b26351
ellorensc <ellorensc@gmail.com>
2017-01-25 23:26:30 +0100

    [Euler] Problem 15 - Lattice Paths

---
+++ src/euler/problem15/LatticePath.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ellorensc
+ * Date: 22/1/17
+ * Time: 19:28
+ */
+class LatticePath
+{
+
+}
---
+++ tests/LatticePathsTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ellorensc
+ * Date: 25/1/17
+ * Time: 23:17
+ */
+
+namespace tests;
+
+
+class LatticePathsTest
+{
+
+}
