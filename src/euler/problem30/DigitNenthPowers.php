4a09983d246796dec184bba0fa0ca5bd9f04dd2b
ellorensc <ellorensc@gmail.com>
2017-02-12 17:51:38 +0100

    [Euler] Problem 30 - Digit fifth powers

---
+++ src/euler/problem30/DigitNenthPowers.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ellorensc
+ * Date: 12/2/17
+ * Time: 11:52
+ */
+class DigitNenthPowers
+{
+
+}
---
+++ tests/DigitNenthPowersTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ellorensc
+ * Date: 12/2/17
+ * Time: 12:22
+ */
+
+namespace tests;
+
+
+class DigitNenthPowersTest
+{
+
+}
