1c09538b17c992a08f9e5b94135416ffe9888564
ellorensc <ellorensc@gmail.com>
2017-03-29 22:32:48 +0200

    [Euler] Problem 33 - Digit Cancelling Fractions

---
+++ src/euler/problem33/DigitCancellingFraction.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: ellorensc
+ * Date: 29/3/17
+ * Time: 22:26
+ */
+class DigitCancellingFraction
+{
+
+}
---
+++ tests/DigitCancellingFractionTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ellorensc
+ * Date: 29/3/17
+ * Time: 22:29
+ */
+
+namespace tests;
+
+
+class DigitCancellingFractionTest
+{
+
+}
